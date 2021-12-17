75d783ee32a96bb8e5377e27a9b5d49c7be16e30
SuYiXuan <3a832012@gm.student.ncut.edu.tw>
2021-12-17 16:24:34 +0800

    更新所有資料表

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,17 +11,17 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
+    #php artisan migrate:fresh 資料庫全部更新
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->id();                                   #編號
+            $table->string('name');                  #姓名
+            $table->string('password');              #密碼
+            $table->string('address');               #地址
+            $table->string('phone');                 #電話
+            $table->date('birthday');                #生日
+            $table->string('email');                 #信箱
             $table->timestamps();
         });
     }
--- database/migrations/2021_12_17_064020_create_details_table.php
+++ database/migrations/2021_12_17_064020_create_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,17 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('details', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('users_id');
             $table->foreign('users_id')->references('id')->on('users');  #會員編號
-            $table->integer('number');   #數量
-            $table->integer('sum');      #總價
-            $table->date('date');        #日期
-            $table->text('status');      #狀態
+
+            $table->unsignedBigInteger('orders_id');
+            $table->foreign('orders_id')->references('id')->on('orders');  #訂單編號
+
+            $table->unsignedBigInteger('products_id');
+            $table->foreign('products_id')->references('id')->on('products');  #產品編號
+
             $table->timestamps();
         });
     }
@@ -32,6 +35,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('details');
     }
 }
