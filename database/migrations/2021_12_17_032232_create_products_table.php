fc145cfd29512b2e9e02f18d9d552770ba5c0210
Chen Pei An <3a832009@gm.student.ncut.edu.tw>
2021-12-17 11:38:09 +0800

    建立Product資料庫

---
+++ database/migrations/2021_12_17_032232_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();                       #編號
+            $table->string('name');            #名稱
+            $table->string('sort');            #類別
+            $table->integer('price');               #價格
+            $table->integer('capacity');            #容量
+            $table->date('date');        #製造日期
+            $table->integer('year');                #釀造年分
+            $table->integer('stock');               #庫存
+            $table->text('introduce');   #介紹
+            $table->text('status');      #狀態
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
